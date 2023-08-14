16c1a811701f225c28988992180d127e6daf36c1
nicokaplan2005 <nicokaplan2005@gmail.com>
2023-08-14 19:10:59 -0300

    Con regon hasta connectedR

---
+++ Region.hs
@@ -0,0 +1,31 @@
+module Region --( Region, newR, foundR, linkR, tunelR, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR )
+   where
+import City
+import Link 
+import Tunel 
+import Point
+import Quality
+
+data Region = Reg [City] [Link] [Tunel]
+
+newR :: [City] -> [Link] -> [Tunel] -> Region
+newR city links tuneles= Reg city links tuneles
+
+foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
+foundR (Reg citys links tuneles) city=Reg (city:citys) links tuneles
+
+linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
+linkR (Reg citys links tuneles) cityA cityB quality= Reg citys (newL cityA cityB quality:links) tuneles
+
+--tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+--este lo tengo q hacer
+
+connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
+connectedR (Reg citys links tuneles) cityA cityB |length([tunel | tunel <- tuneles,connectsT cityA cityB tunel])>0 =True
+                                                                |otherwise = False
+
+
+{-linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades-}
+
