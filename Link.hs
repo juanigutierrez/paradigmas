d8ef0ac6a595430b15567c3cb57ccf7dd6c7f8f1
juanigutierrez <juani1232004@gmail.com>
2023-08-15 12:56:34 -0300

    modificación nico link

--- Link.hs
+++ Link.hs
@@ -14,7 +14,7 @@ newL city1 city2 quality = Lin city1 city2 quality
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
 connectsL city (Lin city1 city2 _) = (city == city1) || (city == city2)
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
-linksL city1 city2 (Lin city3 city4 _) = (city1 == city3 && city2==city4) || (city2 == city3 && city1 == city4)
+linksL city1 city2 link = connectsL city1 link && connectsL city2 link
 capacityL :: Link -> Int
 capacityL (Lin _ _ quality) = capacityQ quality
 delayL :: Link -> Float     -- la demora que sufre una conexion en este canal
