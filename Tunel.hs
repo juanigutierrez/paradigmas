da4d162577823499d22454d4203a014a4259d70d
juanigutierrez <juani1232004@gmail.com>
2023-08-15 00:18:03 -0300

    Suma delays modulo Tunel; modificacion de delayT

--- Tunel.hs
+++ Tunel.hs
@@ -1,4 +1,4 @@
-module Tunel ( Tunel, newT, connectsT, usesT, delayT )
+module Tunel ( Tunel, newT, connectsT, usesT, delayT, delaysT )
    where
 import City
 import Link
@@ -19,4 +19,5 @@ delaysT (Tun links) = [delayL link | link <- links]
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 --en este no entendí si quiere la mayor demora de las conexiones, la suma de todas las demoras, o la demora de una conexión random.
-delayT tunel = maximum (delaysT tunel)
+--delayT tunel = maximum (delaysT tunel)
+delayT tunel = foldr (+) 0 (delaysT tunel)
