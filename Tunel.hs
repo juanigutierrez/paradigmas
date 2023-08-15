1e224d2448545504e7634d205cd9acf7bc8b60bd
juanigutierrez <juani1232004@gmail.com>
2023-08-15 15:27:31 -0300

    Arreglo y simplificación de delayT

--- Tunel.hs
+++ Tunel.hs
@@ -1,4 +1,4 @@
-module Tunel ( Tunel, newT, connectsT, usesT, delayT, delaysT )
+module Tunel ( Tunel, newT, connectsT, usesT, delayT)
    where
 import City
 import Link
@@ -14,10 +14,7 @@ connectsT city1 city2 (Tun links)= length [x | x <- links, linksL city1 city2 x]
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 usesT link (Tun links) = length [x | x<-links, link == x] == 1
 
-delaysT :: Tunel -> [Float]
-delaysT (Tun links) = [delayL link | link <- links]
-
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 --en este no entendí si quiere la mayor demora de las conexiones, la suma de todas las demoras, o la demora de una conexión random.
 --delayT tunel = maximum (delaysT tunel)
-delayT tunel = foldr (+) 0 (delaysT tunel)
+delayT (Tun links) = foldr (+) 0 [delayL link | link <- links]
