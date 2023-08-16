57cb223849f94258971a802f7c14b707faffbb8b
nicokaplan2005 <nicokaplan2005@gmail.com>
2023-08-16 14:33:13 -0300

    juani puto

--- Region.hs
+++ Region.hs
@@ -6,41 +6,33 @@ import Tunel
 import Point
 import Quality
 
+
 data Region = Reg [City] [Link] [Tunel] deriving (Eq,Show)
 
 newR :: Region
 newR = Reg [] [] []
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg citys links tunnels) city = Reg (city:citys) links tunnels
+foundR (Reg citys links tunnels) city | city `elem` citys = error "la ciudad ya se encuentra en la region"
+                                      | otherwise = Reg (city:citys) links tunnels
    --revisar si ya esta incluida
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada.
 linkR (Reg citys links tunnels) cityA cityB quality = Reg citys ((newL cityA cityB quality):links) tunnels
 
-tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-
-   --PREGUNTAR: si quieren que generemos un tunel entre dos ciudades, lo que tenemos que hacer es generar un link y meterlo en el tunel. 
-   --pero para poder usar linkR, necesitamos la calidad de la conexión.
-
---tunelR Reg(citys links tunels) ccity = Reg citys links ((newT ))
+--tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+--lo complicado es chequear que existe un camino entre la primera ciudad y la ultima que se conecten mediante links
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
-connectedR (Reg citys links tuneles) cityA cityB = length([tunel | tunel <- tuneles,connectsT cityA cityB tunel]) > 0
-
+connectedR (Reg _ _ tuneles) cityA cityB = length([tunel | tunel <- tuneles,connectsT cityA cityB tunel]) > 0
 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
-linkedR (Reg citys links tuneles) cityA cityB = length([link | link <- links, linksL cityA cityB link]) > 0 
+linkedR (Reg _ links _) cityA cityB = length([link | link <- links, linksL cityA cityB link]) > 0 
                                                                 
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora.
-   --las ciudades pueden estar conectadas por links y túneles por separado.
-   --Preguntar: deberíamos checkear los links de la lista que estan en los tuneles, los que no estén sumarlos y los que estén sumar lo que esta
-   --adentro del tunel.
---delayR (Reg citys links tuneles) cityA cityB = foldr (+) 0 [delayT tunel | tunel <- tuneles, connectsT cityA cityB tunel]
-   --acá solo estoy sumando las que estan en los tuneles. no estoy conciderando los links que no estan en túneles, pero si los links que no conectan esas ciudades.
-delayR (Reg citys links tuneles) cityA cityB = foldr (+) 0 [delayL link | link <- links, linksL cityA cityB link]
-   --acá solo estoy sumando los links que conectan dos ciudades y son parte de la lista de links.
-   --ver caso donde las ciudades no estan conectadas, que de error.
-   --PREGUNTA: todos los links de los túneles, deben estar en la lista de links, y viceversa<?
+delayR (Reg citys links tuneles) cityA cityB | connectedR (Reg citys links tuneles) cityA cityB  = foldr (+) 0 [delayL link | link <- links, linksL cityA cityB link]
+                                             | otherwise = error "las ciudades no se encuentran conectadas"
 
---availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades-}
+availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades-}
+availableCapacityForR (Reg citys links tuneles) cityA cityB | linkedR  (Reg citys links tuneles) cityA cityB = capacityL (head [link | link <- links, linksL cityA cityB link])
+                                                            | otherwise= error "no existe una link que los conecte"
